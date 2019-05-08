0c7f84f2dadd041ba9e43d6aca71a07537b9c24d
柴俊堃 <chaijunkun@autohome.com.cn>
2019-05-08 14:50:40 +0800

    remove logger in abstract life cycle

--- yugong-core/src/main/java/com/taobao/yugong/applier/SqlServerIncrementRecordApplier.java
+++ yugong-core/src/main/java/com/taobao/yugong/applier/SqlServerIncrementRecordApplier.java
@@ -12,74 +12,76 @@ import com.taobao.yugong.common.model.record.SqlServerIncrementRecord;
 import com.taobao.yugong.common.utils.YuGongUtils;
 import com.taobao.yugong.exception.YuGongException;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 
 import java.util.List;
 
 
+@Slf4j
 public class SqlServerIncrementRecordApplier extends AbstractRecordApplier {
 
-  protected YuGongContext context;
-  private SqlTemplate sqlTemplate;
-  private DbType dbType;
+    protected YuGongContext context;
+    private SqlTemplate sqlTemplate;
+    private DbType dbType;
 
-  public SqlServerIncrementRecordApplier(YuGongContext context) {
-    this.context = context;
-  }
-
-  @Override
-  public void start() {
-    super.start();
-    dbType = YuGongUtils.judgeDbType(context.getTargetDs());
-    if (dbType == DbType.MYSQL) {
-      sqlTemplate = new MysqlSqlTemplate();
-    } else if (dbType == DbType.ORACLE) {
-      sqlTemplate = new OracleSqlTemplate();
-    } else if (dbType == DbType.SQL_SERVER) {
-      sqlTemplate = new SqlServerTemplate();
-    } else {
-      throw new YuGongException("Unsupported dbtype: " + dbType);
+    public SqlServerIncrementRecordApplier(YuGongContext context) {
+        this.context = context;
     }
-  }
 
-  @Override
-  public void stop() {
-    super.stop();
-  }
-
-  @Override
-  public void apply(List<Record> records) throws YuGongException {
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(context.getTargetDs());
-    for (Record record : records) {
-      if (!(record instanceof SqlServerIncrementRecord)) {
-        logger.info("Record {} is not SqlServerIncrementRecord", record);
-        continue;
-      }
-      applyOneByOne((SqlServerIncrementRecord)record, jdbcTemplate);
+    @Override
+    public void start() {
+        super.start();
+        dbType = YuGongUtils.judgeDbType(context.getTargetDs());
+        if (dbType == DbType.MYSQL) {
+            sqlTemplate = new MysqlSqlTemplate();
+        } else if (dbType == DbType.ORACLE) {
+            sqlTemplate = new OracleSqlTemplate();
+        } else if (dbType == DbType.SQL_SERVER) {
+            sqlTemplate = new SqlServerTemplate();
+        } else {
+            throw new YuGongException("Unsupported dbtype: " + dbType);
+        }
     }
-  }
 
-  private void applyOneByOne(SqlServerIncrementRecord records, JdbcTemplate jdbcTemplate) {
-    String sql = "";
-    switch (records.getOperation()) {
-      case INSERT:
-        sql = sqlTemplate.getInsertSql("", "", new String[]{"a"},
-            Lists.newArrayList("").toArray(new String[]{}));
-        break;
-      case DELETE:
-        sql = sqlTemplate.getDeleteSql("", "", new String[]{"a"});
-        break;
-      case UPDATE_OLD_VALUE:
-        break;
-      case UPDATE_NEW_VALUE:
-        sql = sqlTemplate.getUpdateSql("", "", new String[]{"a"},
-            Lists.newArrayList("").toArray(new String[]{}));
-        break;
+    @Override
+    public void stop() {
+        super.stop();
     }
-    jdbcTemplate.execute(sql, (PreparedStatementCallback) ps-> {
-      return null;
-    });
-    
-  }
+
+    @Override
+    public void apply(List<Record> records) throws YuGongException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(context.getTargetDs());
+        for (Record record : records) {
+            if (!(record instanceof SqlServerIncrementRecord)) {
+                log.info("Record {} is not SqlServerIncrementRecord", record);
+                continue;
+            }
+            applyOneByOne((SqlServerIncrementRecord) record, jdbcTemplate);
+        }
+    }
+
+    private void applyOneByOne(SqlServerIncrementRecord records, JdbcTemplate jdbcTemplate) {
+        String sql = "";
+        switch (records.getOperation()) {
+            case INSERT:
+                sql = sqlTemplate.getInsertSql("", "", new String[]{"a"},
+                        Lists.newArrayList("").toArray(new String[]{}));
+                break;
+            case DELETE:
+                sql = sqlTemplate.getDeleteSql("", "", new String[]{"a"});
+                break;
+            case UPDATE_OLD_VALUE:
+                break;
+            case UPDATE_NEW_VALUE:
+                sql = sqlTemplate.getUpdateSql("", "", new String[]{"a"},
+                        Lists.newArrayList("").toArray(new String[]{}));
+                break;
+        }
+        jdbcTemplate.execute(sql, (PreparedStatementCallback) ps -> {
+            return null;
+        });
+
+    }
 }
